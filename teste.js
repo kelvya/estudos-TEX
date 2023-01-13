b512d238a0c168bd74a0c20ceff00e77b24317dc
kelvya <kelvya.kt@gmail.com>
2023-01-12 22:31:52 -0300

    Atualizações pontuais

---
+++ teste.js
@@ -0,0 +1,12 @@
+const x = y = z = 50;
+console.log(x, y, z);
+
+const myVar = 12;
+const myvar = 13;
+
+let a = 10;
+a &= 3;// 10 & 3
+console.log(a)
+
+const ola = 'Ola mundo!'
+console.log(ola.toUpperCase(), ola.toLowerCase(), ola.indexOf('u'), ola.substring(1));
