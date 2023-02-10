3ca855f371abc1ee7a0021a75c45d48b3ecc37c2
kelvya <kelvya.kt@gmail.com>
2023-02-10 12:48:35 -0300

    Atualização da aula 3.8.3 – Criando Rotas com Express

--- nodeJs/codigos/meuApp/index.js
+++ nodeJs/codigos/meuApp/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.end('Iniciando meu primeiro app em Node.js com Express :)')
-})
+// Exemplo usado na aula 3.8.2 – Framework Express
+// app.get('/', (req, res) => {
+//     res.end('Iniciando meu primeiro app em Node.js com Express :)')
+// })
+
+
+app.get("/", (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
+    res.end("Iniciando meu primeiro app em Node.js com Express :)");
+});
+
+app.get("/sobre", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+  res.end("Sobre nosso App");
+});
+
+app.get("/contato", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+  res.end("Entre em contato conosco");
+});
 
 app.listen(3000, ()=>{
     console.log('Server em atividade!')
--- nodeJs/nodeJs.md
+++ nodeJs/nodeJs.md
@@ -4,5 +4,7 @@
 
 [3.8.2 – Framework Express](/nodeJs/markdown/aula-1.8.2.md)
 
-[](/nodeJs/markdown/aula-1.8.3.md)
+[3.8.3 – Criando Rotas com Express](/nodeJs/markdown/aula-1.8.3.md)
+
+[](/nodeJs/markdown/aula-1.8.4.md)
 
