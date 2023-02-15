5da58400c603c1604b7346dc3eb981778cb4c473
kelvya <kelvya.kt@gmail.com>
2023-02-14 21:05:31 -0300

    Correção em arquivos do aulao 2 de Node.js

--- nodeJs/aulao-node/upload.js
+++ nodeJs/aulao-node/upload.js
@@ -7,7 +7,7 @@ http.createServer((req, res) =>{
         const form = new formidable.IncomingForm()
         form.parse(req, (err, fields, files) =>{
             const oldPath = files.upload.filepath
-            const newPath = `${__dirname}/imagens${files.upload.originalFilename}`
+            const newPath = `${__dirname}/imagens/${files.upload.originalFilename}`
 
             fs.rename(oldPath, newPath, (err)=>{
                 if(err) throw err
@@ -18,8 +18,8 @@ http.createServer((req, res) =>{
         res.writeHead(200, {'Content-type':"text/html"})
         res.write('<form action="upload" method="post" enctype="multipart/form-data">')
         res.write('<input type="file" name="upload"><br/>')
-        res.write('<button>EnviarArquivo</button>')
-        res.write('<form')
+        res.write('<button>Enviar Arquivo</button>')
+        res.write('<form>')
         return res.end()
     }
 }).listen(3000)
